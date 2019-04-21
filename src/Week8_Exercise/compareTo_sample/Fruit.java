ce1ed5d1e51aaedd53f5772ef496a6a9a686ddd7
jeonyeohun <21500630@handong.edu>
2019-04-21 20:30:45 +0900

    Update Fruit.java

--- src/Week8_Exercise/compareTo_sample/Fruit.java
+++ src/Week8_Exercise/compareTo_sample/Fruit.java
@@ -1,4 +1,4 @@
-public class Fruit{
+public class Fruit implements Comparable{
   private String fruitName;
   public Fruit(){
     fruitName = "";
@@ -11,5 +11,13 @@ public class Fruit{
   }
   public String getName(){
     return fruitName;
+  }
+
+  public int compareTo (object o){
+    if ((o != null) && (o instanceof Fruit)){ // instanceof >> 형번환이 가능한지 알아보기. o가 Fruit 를 상속하는 객체라면 true 반환
+      Fruit otherFruit = (Fruit) o;
+      return (fruitName.compareTo(otherFruit.fruitName));
+    }
+    return -1;
   }
 }
