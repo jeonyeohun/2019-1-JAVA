0866d780fdfb13f0b37b3064a849659b60a79acc
jeonyeohun <21500630@handong.edu>
2019-04-29 10:41:26 +0900

    Week10 works

---
+++ src/Week10_Exercise/[01]TextFileOutputDemo/TextFileOutputDemo.java
@@ -0,0 +1,22 @@
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class TextFileOutputDemo{
+  public static void main (String[] args){
+    String filename = "out.txt";
+    PrintWriter outputStream = null;
+    try{outputStream = new PrintWriter(filename);}
+    catch (FileNotFoundException e){
+      System.out.println("Error opening the file" + filename);
+      System.exit(0);
+    }
+    System.out.println("Enter three lines of text: ");
+    Scanner keyboard = new Scanner (System.in);
+    for (int count =1; count <=3 ; count++){
+      String line = keyboard.nextLine();
+      outputStream.println(count + " " + line);
+    }
+    outputStream.close();
+    System.out.println("Those lines were written to " + filename);
+  }
+}
